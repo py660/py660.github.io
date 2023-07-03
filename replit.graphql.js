8601dac40bd3996f1fbb3b2b0c603661b5cdce36
py660 <93660666+py660@users.noreply.github.com>
2023-07-02 23:05:12 -0400

    Create replit.graphql.js

---
+++ replit.graphql.js
@@ -0,0 +1,11 @@
+self.addEventListener('fetch', function(event) {
+    const newRequest = new Request(event.request, {
+        headers: {
+                    "X-Requested-With": "ReplitApi",
+                    "referer": "https://replit.com/",
+                    "User-Agent": "Mozilla/5.0"
+                  },
+        mode: "cors"
+    });
+    return fetch(newRequest);
+}
